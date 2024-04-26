abf3594074832f95dac74168fbae77f0a2f89052
minseok <minskyson7@gmail.com>
2024-04-26 20:06:06 +0900

    수정

--- movie.js
+++ movie.js
@@ -1,1 +1,21 @@
-console.log("안녕하세요")
+const options = {
+  method: "GET",
+  headers: {
+    accept: "application/json",
+    Authorization:
+      "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzODBjNWU5ZDM0YTU2M2I5NTMxYTRlYjllNDU1ODMyYyIsInN1YiI6IjY2MmIzOTk4ZjkxODNhMDExYzMwZmI2ZiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.QWhcRuNHIniKPBKvi072GEb21l7F25suoW5xmvUB5vg",
+  },
+};
+let moviedata;
+fetch(
+  "https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1",
+  options
+)
+  .then((response) => response.json())
+  .then((response) => console.log(response))
+  .catch((err) => console.error(err));
+  
+// const movielist = document.querySelector('.movie')
+response.forEach(a => {
+  
+});

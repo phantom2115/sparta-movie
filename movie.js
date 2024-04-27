b9a4f2aee3da5644effff260225ca0ad817b4dbb
minseok <minskyson7@gmail.com>
2024-04-28 03:32:39 +0900

    id 검색 기능

--- movie.js
+++ movie.js
@@ -6,45 +6,55 @@ const options = {
       "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzODBjNWU5ZDM0YTU2M2I5NTMxYTRlYjllNDU1ODMyYyIsInN1YiI6IjY2MmIzOTk4ZjkxODNhMDExYzMwZmI2ZiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.QWhcRuNHIniKPBKvi072GEb21l7F25suoW5xmvUB5vg",
   },
 };
-let moviedata;
+// let moviedata;
+
 fetch(
   "https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1",
   options
 )
   .then((response) => response.json())
-  .then((response) => {
-    console.log(response)
-    response.results.forEach(element => {
-      
+  .then((response) => {    
     
+    response.results.forEach(element => {    
      let  movie= {
       poster : element.backdrop_path,
       title : element.title,
       view : element.overview,
-      average : element.vote_average
-    }
-   
-    let card = `<div id = "card" >
-                <img src="https://image.tmdb.org/t/p/w200/${movie.poster}" alt="">
-                <p id = "title">${movie.title} </p>
-                <p id = "view">${movie.view}</p>
-                <p>rating :  ${movie.average}</p>
+      average : element.vote_average,
+      id : element.id
+
+    }  
+    let card = `<div id = "card" onclick = "search(${movie.id})" > 
+                 <img id = "poster"src="https://image.tmdb.org/t/p/w200/${movie.poster}" alt="">
+                  <div id = "incard">
+                    <p id = "title">${movie.title} </p>
+                    <p id = "view">${movie.view}</p>
+                    <p>⭐️ :  ${movie.average}</p>
+                  </div>
                 </div>`
+            
     const movielist = document.querySelector('.movie')
-    console.log(card)
-    movielist.innerHTML += card;
+    movielist.innerHTML += card;  
     
     
-  });
+    });
+    
+  })
+  .catch(err => console.error(err));
+
+  function search(movieId) {
+    alert('id: ' + movieId); 
   }
-)
 
-
-//사진 제목 내용 평점
+  window.onload = function() {
+    document.getElementById("text-search").focus();
+  };
 
   
-  .catch((err) => console.error(err));
-
   
 
 
+
+//검색 기능
+
+  
